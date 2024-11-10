f2cd908036c19b051f67a5bc4f60eee50d5fa2a3
Waqar <waqar-1@hotmail.co.uk>
2024-11-10 02:54:37 +0000

    learnt strings

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,24 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        // hello world
         System.out.println("Hello World! its me, Waqar");
+
+        // primitive datatypes
+        int studentAge = 16;
+        double studentGPA = 3.4;
+        char studentFirstInitial = 'W';
+        char studentLastInitial = 'F';
+        boolean hasPerfectAttendance = true;
+        String studentFirstName = "Waqar";
+        String studentLastName = "Farooq";
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+
     }
 }
